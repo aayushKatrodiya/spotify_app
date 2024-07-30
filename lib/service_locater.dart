9d65a657d2232a17197e30d5104e7f18c9936e23
aayushKatrodiya <aayushkatrodiya8460@gmail.com>
2024-07-30 19:23:07 +0530

    done

--- lib/service_locater.dart
+++ lib/service_locater.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:spotify_app2/data/repository/auth/auth_repository_impl.dart';
 import 'package:spotify_app2/data/sources/auth/auth_firebase_service.dart';
 import 'package:spotify_app2/domain/repository/auth/auth.dart';
+import 'package:spotify_app2/domain/usecases/auth/signup.dart';
 
 final sl = GetIt.instance;
 
@@ -12,5 +13,8 @@ Future<void> initiolizeDependencies() async {
 
   sl.registerSingleton<AuthRepository>(
     AuthRepositoryImpl(),
+  );
+  sl.registerSingleton<SignupUsecase>(
+    SignupUsecase(),
   );
 }
